2afaedda0a663e4db838dd131d99a27bd186083d
Dobromir Zhivkov <dobromirzhivkovv@gmail.com>
2023-09-18 21:05:39 +0300

    feat: Create FeaturesSection component and add it to Homepage

--- client/src/components/Homepage.jsx
+++ client/src/components/Homepage.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import Lottie from 'lottie-react';
 import animationData from '../assets/animation_lm3tdfmc.json';
+import hobbiesImg from '../assets/featuresImages/hobbies.png' 
+import friendImg from '../assets/featuresImages/friend.png'
+import conversationImg from '../assets/featuresImages/conversation.png'
+
 const HeroSection = () => {
     return(
         <div className='flex flex-col lg:flex-row h-[90%] w-full'>
@@ -17,10 +21,40 @@ const HeroSection = () => {
     )
 }
 
+const FeaturesSection = ({ children, feature, image, isReversed = false }) => {
+    // Conditionally set the class name based on the `reverse` prop
+    const containerClassName = `py-10 flex flex-col lg:flex-row w-[95%] text-center justify-evenly items-center ${isReversed ? 'lg:flex-row-reverse' : ''}`;
+  
+    return (
+      <div className={containerClassName}>
+        <div>
+            <img src={image} className='w-[500px]' alt={`${feature}`} />
+        </div>
+        <div>
+          <h2 className='text-2xl font-bold'>{feature}</h2>
+            {children}
+        </div>
+      </div>
+    );
+}
+
 const Home = () => {
     return(
         <div className='h-screen'>
         <HeroSection/>
+        <div className='w-full'>
+            <FeaturesSection feature='Discover and connect' image={hobbiesImg}>
+                <p>Browse through a diverse selection of chatrooms to find your niche. </p>
+            </FeaturesSection>
+            <FeaturesSection feature='Communicate freely' image={conversationImg}>
+                <p>Enjoy conversations with friends and strangers.</p> 
+            </FeaturesSection>
+            <FeaturesSection feature='Make new friends' image={friendImg} isReversed={true}>
+                <p>Make friendships that last for a lifetime. </p>
+            </FeaturesSection>
+        </div>
         </div>
     )
 }
+ 
+export default Home;
