6fbaaff84a823ed134defc1f1e83df13de7518ce
Dobromir Zhivkov <dobromirzhivkovv@gmail.com>
2023-09-18 20:49:11 +0300

    feat: Add React Router setup and AppRoutes component

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,12 @@
+import React from 'react';
+import { BrowserRouter as Router} from 'react-router-dom';
+import AppRoutes from './routes/AppRoutes.jsx';
+
 function App() {
   return (
-    <div>
-    </div>
+    <Router>
+        <AppRoutes/>
+    </Router>
   );
 }
 
