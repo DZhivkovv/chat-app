579b8f4dc8b65ce052d7e02b43129240f9875a2b
Dobromir Zhivkov <dobromirzhivkovv@gmail.com>
2023-09-20 15:17:50 +0300

    feat: Create UserCredentialsForm component

---
+++ client/src/components/authentication/UserCredentialsForm.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import Input from '../Input';
+import useAuthFields from '../../hooks/authentication/useAuthFields';
+
+// A form component used in Login and Signup pages
+const UserCredentialForm = ({ children, handleSubmit }) => {
+    // Use the custom hook to manage email and password fields
+    const { email, setEmail, password, setPassword } = useAuthFields();
+
+    return (
+        <form
+            onSubmit={(event) => {
+                handleSubmit(event, email, password);
+            }}
+            className="w-full max-w-xs"
+        >
+            {/* Input field for email */}
+            <Input
+                type="email"
+                placeholder="Email"
+                value={email}
+                onChange={(event) => {
+                    // Update the email state when the input changes
+                    setEmail(event.target.value);
+                }}
+                className="mb-4"
+            />
+
+            {/* Input field for password */}
+            <Input
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(event) => {
+                    // Update the password state when the input changes
+                    setPassword(event.target.value);
+                }}
+                className="mb-6"
+            />
+
+            {/* Submit button */}
+            <button
+                className="bg-blue-300 text-white py-2 px-4 rounded-lg transition-colors hover:bg-blue-200"
+                type="submit"
+            >
+                {/* Display the content passed as children inside the button */}
+                <p className='font-bold'>{children}</p>
+            </button>
+        </form>
+    );
+};
+
+export default UserCredentialForm;
