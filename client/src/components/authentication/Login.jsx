f5825e2143ccef3979d97a3705e50354e07c2949
Dobromir Zhivkov <dobromirzhivkovv@gmail.com>
2023-09-20 15:24:55 +0300

    Create Login component that allows the users to log into their accounts in the app

---
+++ client/src/components/authentication/Login.jsx
@@ -0,0 +1,39 @@
+import useLogin from '../../hooks/authentication/useLogin';
+import WelcomePanel from './WelcomePanel'; 
+import UserCredentialsForm from './UserCredentialsForm';
+import Button from '../Button'; 
+import { Link } from 'react-router-dom';
+import { AiFillGoogleCircle } from 'react-icons/ai';
+
+const Login = () => {
+    const { login, loginWithGoogle } = useLogin(); //Functions to login with email/password and with Google
+
+    return (
+        <div className="flex h-screen">     
+            {/* Left part of the page. */}
+            <WelcomePanel/> 
+
+            {/* Right part of the page. */}            
+            <div className="flex flex-col h-full justify-center items-center p-8">
+                <h1 className="text-3xl font-bold mb-6">Login</h1> 
+                
+                {/* Login form */}
+                <UserCredentialsForm handleSubmit={login}> Login </UserCredentialsForm>
+                <p className="text-center text-gray-600 mt-4">Or use</p> 
+                
+                {/* A button for Google login. */}
+                <Button type="button" handleClick={loginWithGoogle} className="mt-4 bg-blue-600 hover:bg-blue-700"> 
+                    <AiFillGoogleCircle className='text-3xl text-gray-500  hover:text-blue-400'/>
+                </Button>
+                
+                {/*Link to signup page */}
+                <div className='mt-5 flex flex-wrap justify-center'> 
+                    <p className='pr-2 block'>Don't have an account yet?</p> 
+                    <Link to='/signup' className='font-bold text-blue-500'>Create an account</Link> 
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Login; 
