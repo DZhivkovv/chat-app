c175884fc6d34e918b2176cb4de86c11b30a028d
Dobromir Zhivkov <dobromirzhivkovv@gmail.com>
2023-09-20 15:29:59 +0300

    chore: Add routes to Login and Signup pages

--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import {Routes, Route} from 'react-router-dom';
 import Homepage from '../components/Homepage'
+import Login from '../components/authentication/Login';
+import Signup from '../components/authentication/Signup';
 
 const AppRoutes = () => {
     return(
         <Routes>
             <Route path="/" element={<Homepage />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
         </Routes>
     )    
 }
