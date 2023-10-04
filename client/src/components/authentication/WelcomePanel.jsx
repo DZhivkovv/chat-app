00b8c87bf4487e53f1ffd2bcc51067937e5436e9
Dobromir <dobromirzhivkovv@gmail.com>
2023-10-04 12:54:19 +0300

    style: Add styling to WelcomePanel component in login and signup pages

--- client/src/components/authentication/WelcomePanel.jsx
+++ client/src/components/authentication/WelcomePanel.jsx
@@ -1,23 +1,19 @@
+import React from 'react';
 import { Link } from 'react-router-dom';
-import {BiArrowBack} from 'react-icons/bi'
+import { BiArrowBack } from 'react-icons/bi';
 
-
-// A styled welcome panel displayed in the login and signup pages.
 const WelcomePanel = () => {
   return (
-    <div className="hidden lg:block lg:w-2/3 h-screen bg-diagonal-stripes ">
-        {/* A button navigating to the homepage */}
-        <Link to='/'>
-            <BiArrowBack className='text-3xl m-5'/>
+    <div className="hidden lg:block lg:w-2/3 h-screen bg-blue-900 text-white p-8">
+      <div className="text-center">
+        <Link to='/' className="text-white hover:text-gray-300">
+          <BiArrowBack className='text-3xl m-5' />
         </Link>
-        
-        {/* A header and welcome slogan */}
-        <div className="text-center">
-            <h1 className="text-4xl font-bold mb-4">Welcome to Sedyanka</h1>
-            <p className="text-gray-700">Your destination for something great!</p>
-        </div>
+        <h1 className="text-4xl font-bold mb-4">Welcome to Sedyanka</h1>
+        <p className="text-gray-400 text-lg">Your destination for something great!</p>
+      </div>
     </div>
-)
-}
+  );
+};
 
-export default WelcomePanel
+export default WelcomePanel;
